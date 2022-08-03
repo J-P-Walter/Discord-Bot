edc24cd87ac250c78b66da12861ee51825521f89
JP <j.p.walter@outlook.com>
2022-08-03 15:29:25 -0400

    Messed around with command description/brief

--- main_commands.py
+++ main_commands.py
@@ -7,7 +7,19 @@ bot = commands.Bot(command_prefix='!')
 @commands.command()
 async def ping(ctx):
     await ctx.send("Pong")
+
+@commands.command(description="Provide arguments", brief="Repeats message")
+async def hello(ctx, *args):
+    if len(args) > 0:
+        await ctx.send(" ".join(args))
+    else:
+        await ctx.send("Error: please refer to !help")
+
+
+
+
 bot.add_command(ping)
+bot.add_command(hello)
 
 config = configparser.ConfigParser()
 config.read('token.cfg')
