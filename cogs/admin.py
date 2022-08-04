c7a7a1013affb402cd72ca521f80bb4e9a6a828b
JP <j.p.walter@outlook.com>
2022-08-04 15:45:44 -0400

    Added some admin only functionality

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,12 +1,24 @@
 from discord.ext import commands
 import discord
 import datetime
+from settings._global import MOD_ROLE_NAME
+
+def mods_or_owner():
+    def predicate(ctx):
+        return commands.check_any(commands.is_owner(), commands.has_role(MOD_ROLE_NAME))
+    return commands.check(predicate)
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
+    @mods_or_owner()
+    async def foo(self, ctx):
+        await ctx.send("A")
+
+    @commands.command()
+    @commands.is_owner()
     async def status(self, ctx):
         guild = ctx.guild
         num_voice_channels = len(guild.voice_channels)
@@ -19,6 +31,9 @@ class Admin(commands.Cog):
         embed.set_author(name=self.bot.user.name)
         embed.add_field(name="Custom Emoji", value=guild.emojis[0] or "", inline=False)
         embed.set_image(url="https://i1.sndcdn.com/artworks-doULutRNuGLb4ou0-71NpIA-t500x500.jpg")
+        embed.add_field(name="AFK Channel", value=guild.afk_channel)
+        
+        
         await ctx.send(embed=embed)
 
 def setup(bot):
